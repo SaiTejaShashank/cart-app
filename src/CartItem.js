7458735fea989888481b861292df224170387d47
ananthu <anandu@Ananthan-MacBook-Pro.local>
2023-04-05 16:29:15 +0530

    add increase quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,38 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+  constructor () {
+    super();
+    //this.increaseQuantity=this.increaseQuantity.bind(this);
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: ''
+    }
+  }
+
+  increaseQuantity(){
+    console.log('this.state',this);
+  }
   render () {
+    const { price, title, qty } = this.state;
+
+
     return (
       <div className="cart-item">
         <div className="left-block">
           <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={ { fontSize: 25 } }>Phone</div>
-          <div style={ { color: '#777' } }>Rs 999</div>
-          <div style={ { color: '#777' } }>Qty: 1</div>
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
           <div className="cart-item-actions">
             {/* Buttons */}
+            <img alt="increase" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/04/91/21/35/1000_F_491213526_YSg0K2hbLWtlRzcwubYVkMt3lxAi7sD4.jpg" onClick={this.increaseQuantity.bind(this)} />
+            <img alt="decrease" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/04/91/21/35/1000_F_491213556_mkPmpdbaTvDYYEbNNCHntfKC9XC51W6j.jpg" />
+            <img alt="delete" className="action-icons" src="https://as1.ftcdn.net/v2/jpg/04/92/30/88/1000_F_492308833_xXc7hxGdBrk3OQtb9NKCKq0s1hZ40PC6.jpg" />
           </div>
         </div>
       </div>
