0913fca730a5250e225794cb807108c5ab4ac89f
ananthu <anandu@Ananthan-MacBook-Pro.local>
2023-04-05 16:43:34 +0530

    setstate increase decrease quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,9 +11,33 @@ class CartItem extends React.Component {
       img: ''
     }
   }
+  increaseQuantity = () => {
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // });
 
-  increaseQuantity(){
-    console.log('this.state',this);
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
+  }
+
+  decreaseQuantity=()=>{
+
+    const {qty}=this.state;
+    if(qty==0){
+        return;
+    }
+    this.setState((prevState)=>{
+        return {
+            qty:prevState.qty-1
+        }
+    });
   }
   render () {
     const { price, title, qty } = this.state;
@@ -30,8 +54,8 @@ class CartItem extends React.Component {
           <div style={ { color: '#777' } }>Qty: {qty} </div>
           <div className="cart-item-actions">
             {/* Buttons */}
-            <img alt="increase" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/04/91/21/35/1000_F_491213526_YSg0K2hbLWtlRzcwubYVkMt3lxAi7sD4.jpg" onClick={this.increaseQuantity.bind(this)} />
-            <img alt="decrease" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/04/91/21/35/1000_F_491213556_mkPmpdbaTvDYYEbNNCHntfKC9XC51W6j.jpg" />
+            <img alt="increase" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/04/91/21/35/1000_F_491213526_YSg0K2hbLWtlRzcwubYVkMt3lxAi7sD4.jpg" onClick={this.increaseQuantity} />
+            <img alt="decrease" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/04/91/21/35/1000_F_491213556_mkPmpdbaTvDYYEbNNCHntfKC9XC51W6j.jpg" onClick={this.decreaseQuantity}/>
             <img alt="delete" className="action-icons" src="https://as1.ftcdn.net/v2/jpg/04/92/30/88/1000_F_492308833_xXc7hxGdBrk3OQtb9NKCKq0s1hZ40PC6.jpg" />
           </div>
         </div>
